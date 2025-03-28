6482bdfcfc0ccaa74932d51b08c86d903180357a
arkanoider <github.913zc@simplelogin.com>
2025-03-28 22:54:25 +0100

    Now creates a plaintext db, encrypts it and then decrypt

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,99 @@
-use std::{fs, io::{Read, Write}, str::FromStr};
+use core::str;
+use std::{
+    fs,
+    io::{Read, Write},
+    path::Path,
+    str::FromStr,
+};
 
 use argon2::{
-    password_hash::{
-        rand_core::OsRng, PasswordHasher, Salt, SaltString
-    },
-    Argon2, PasswordHash, PasswordVerifier
+    password_hash::{rand_core::OsRng, PasswordHasher, Salt, SaltString},
+    Argon2, PasswordHash,
 };
-use chacha20poly1305::{aead::AeadMut, ChaCha20Poly1305, Key, Nonce};
-use zeroize::Zeroize;
-use secrecy::{ExposeSecret, SecretString};  
-use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
 use chacha20poly1305::aead::{Aead, AeadCore, KeyInit};
+use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
+use secrecy::{ExposeSecret, SecretString};
+use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
+use zeroize::Zeroize;
 
+pub const SALT_SIZE: usize = 22;
+pub const NONCE_SIZE: usize = 12;
 
-// fn decrypt_database(password: &SecretString, salt: &SaltString, decrypted_db_file_path: &str, encrypted_db_file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
-//     // 1. Read the entire database file into memory
-//     let mut encrypted_db_file = fs::File::open(encrypted_db_file_path)?;
-//     let mut encrypted_data = Vec::new();
-//     encrypted_db_file.read_to_end(&mut encrypted_data)?;
+fn decrypt_database(
+    password: &SecretString,
+    decrypted_db_file_path: &str,
+    encrypted_db_file_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // 1. Read the entire database file into memory
+    let mut encrypted_db_file = fs::File::open(encrypted_db_file_path)?;
+    let mut encrypted_data = Vec::new();
+    encrypted_db_file.read_to_end(&mut encrypted_data)?;
 
+    let keyderived = password.expose_secret();
+    let nonce = Nonce::from_slice(&encrypted_data[SALT_SIZE..SALT_SIZE + NONCE_SIZE]);
+    println!("Nonce: {:?}", nonce);
+    println!("Nonce len: {:?}", nonce.len());
 
-//     // 2. Parse the Salt and Ciphertext from the Encrypted File
-//     let salt = &encrypted_data[0..SALT_SIZE];
-//     let nonce = Nonce::from_slice(&encrypted_data[SALT_SIZE..SALT_SIZE + NONCE_SIZE]);
-//     let ciphertext = &encrypted_data[SALT_SIZE + NONCE_SIZE..];
+    let ciphertext = &encrypted_data[SALT_SIZE + NONCE_SIZE..];
 
+    // 3. Encrypt the Database
+    println!("Key Derived: {:?}", keyderived);
+    let parsed_hash = PasswordHash::new(keyderived).map_err(|_| "Error in password check")?;
 
-//     let cipher = ChaCha20Poly1305::new(Key::from_slice(key_bytes));
-//     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
-//     let ciphertext = cipher.decrypt(&nonce, db_data.as_slice()).map_err(|_| "Error encrypting database")?;
+    let key = parsed_hash.hash.unwrap();
+    let key_bytes = key.as_bytes();
+    if key_bytes.len() != 32 {
+        panic!("Key length is not 32 bytes");
+    }
+    println!("Key Bytes: {:?}", key_bytes);
 
-//     // 4. Write the Salt and Ciphertext to the Encrypted File
-//     let mut encrypted_file = fs::File::create(decrypted_db_file_path)?;
-//     encrypted_file.write_all(salt.as_str().as_bytes())?; // Store the salt string
-//     encrypted_file.write_all(&ciphertext)?;
+    let cipher = ChaCha20Poly1305::new(Key::from_slice(key_bytes));
+    let plaintext = cipher
+        .decrypt(nonce, ciphertext)
+        .map_err(|_| "Error encrypting database")?;
 
-//     Ok(())
-// }
+    // 4. Write the Salt and Ciphertext to the Encrypted File
+    let mut decrypted_file = fs::File::create(decrypted_db_file_path)?;
+    decrypted_file.write_all(&plaintext)?;
 
-fn encrypt_database(password: &SecretString, salt: &SaltString, db_file_path: &str, encrypted_db_file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
+
+fn encrypt_database(
+    password: &SecretString,
+    salt: &SaltString,
+    db_file_path: &str,
+    encrypted_db_file_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     // 1. Read the entire database file into memory
     let mut db_file = fs::File::open(db_file_path)?;
     let mut db_data = Vec::new();
     db_file.read_to_end(&mut db_data)?;
 
-
     // 3. Encrypt the Database
-    let parsed_hash = PasswordHash::new(&password.expose_secret()).map_err(|_| "Error in password check")?;
-    
-    let key = parsed_hash.hash.unwrap();
-    if key.len() != 32 {
-        panic!("Key length is not 32 bytes");
-    }
+    let parsed_hash =
+        PasswordHash::new(password.expose_secret()).map_err(|_| "Error in password check")?;
 
+    let key = parsed_hash.hash.unwrap();
     let key_bytes = key.as_bytes();
     if key_bytes.len() != 32 {
         panic!("Key length is not 32 bytes");
     }
+
+    println!("Key Byte: {:?}", key_bytes);
+
     let cipher = ChaCha20Poly1305::new(Key::from_slice(key_bytes));
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
+    println!("Nonce: {:?}", nonce);
     println!("Nonce len: {:?}", nonce.len());
-    let ciphertext = cipher.encrypt(&nonce, db_data.as_slice()).map_err(|_| "Error encrypting database")?;
+    let ciphertext = cipher
+        .encrypt(&nonce, db_data.as_slice())
+        .map_err(|_| "Error encrypting database")?;
 
     // 4. Write the Salt and Ciphertext to the Encrypted File
     let mut encrypted_file = fs::File::create(encrypted_db_file_path)?;
     encrypted_file.write_all(salt.as_str().as_bytes())?; // Store the salt string
+    encrypted_file.write_all(&nonce)?;
     encrypted_file.write_all(&ciphertext)?;
 
     Ok(())
@@ -81,7 +110,7 @@ async fn create_database(db_file_path: &str) -> Result<(), sqlx::Error> {
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL,
             age INTEGER
-        )"
+        )",
     )
     .execute(&pool)
     .await?;
@@ -96,15 +125,19 @@ async fn create_database(db_file_path: &str) -> Result<(), sqlx::Error> {
     Ok(())
 }
 
-
 // Helper function to derive the key and nonce from the password and salt
-fn derive_key(password: &SecretString) -> Result<(String,SaltString), Box<dyn std::error::Error>> {
-
+fn derive_key(
+    password: &SecretString,
+    salt: Option<SaltString>,
+) -> Result<(String, SaltString), Box<dyn std::error::Error>> {
+    //Create argon2 instance
     let argon2 = Argon2::default();
-
-    println!("Argon2 Config: {:?}", argon2);
-    //Create salt
-    let salt = SaltString::generate(&mut OsRng);
+    //Create salt if not provided
+    let salt = if let Some(salt) = salt {
+        salt
+    } else {
+        SaltString::generate(&mut OsRng)
+    };
     println!("Salt: {:?}", salt);
     println!("Salt len: {:?}", salt.len());
 
@@ -114,35 +147,71 @@ fn derive_key(password: &SecretString) -> Result<(String,SaltString), Box<dyn st
     println!("Salt decoded: {:?}", salt_decoded);
     println!("Salt decoded lenght: {:?}", salt_decoded.len());
 
+    println!("Password: {:?}", password.expose_secret().as_bytes());
 
     // Use expose_secret() to access the underlying string data
-    let password_hash = argon2.hash_password(password.expose_secret().as_bytes(), &salt).map_err(|_| "Error hashing password")?.to_string();
+    let password_hash = argon2
+        .hash_password(password.expose_secret().as_bytes(), &salt)
+        .map_err(|_| "Error hashing password")?
+        .to_string();
 
+    println!("Password hash: {:?}", password_hash);
     Ok((password_hash, salt))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // 1️⃣ password input from user
+    // Password input from user
     let mut password = String::from("my_super_secret_password");
     let db_file_path = "mostro.db";
     let encrypted_db_file_path = "mostro-encrypted.db";
     let decrypted_db_file_path = "mostro-decrypted.db";
-    // 2️⃣ Create the database
-    create_database(db_file_path).await?;
 
-    // 3️⃣ Derive a secret key from the password
-    let (argon2_password, argon_salt ) = derive_key(&SecretString::from(password)).map_err(|_| "Error deriving key")?;
+    // Create the database if encrypted file does not exist
+    let (argon2_password, argon_salt) = if !Path::new(encrypted_db_file_path).exists() {
+        create_database(db_file_path).await?;
+        derive_key(&SecretString::from(password.clone()), None).map_err(|_| "Error deriving key")?
+    }
+    // If encrypted file exists, derive the key from the password and salt saved in the file
+    else {
+        println!("Database already exists");
+        let mut encrypted_db_file = fs::File::open(encrypted_db_file_path)?;
+        let mut encrypted_data = Vec::new();
+        encrypted_db_file.read_to_end(&mut encrypted_data)?;
 
+        // 2. Parse the Salt and Ciphertext from the Encrypted File
+        let salt = match str::from_utf8(&encrypted_data[0..SALT_SIZE]) {
+            Ok(salt) => salt,
+            Err(e) => {
+                return Err(e.into());
+            }
+        };
+        derive_key(
+            &SecretString::from(password.clone()),
+            Some(SaltString::from_b64(salt).unwrap()),
+        )
+        .map_err(|_| "Error deriving key")?
+    };
+
+    // Derive a secret key from the password
     println!("Generated key: {:?}", argon2_password);
+    // Immediately zeroize the password from the RAM
+    password.zeroize();
 
-    // 4️⃣ Encrypt the database
-    let _ = encrypt_database(&SecretString::from(argon2_password), &argon_salt, db_file_path, encrypted_db_file_path);
+    // Encrypt the database
+    let _ = encrypt_database(
+        &SecretString::from(argon2_password.clone()),
+        &argon_salt,
+        db_file_path,
+        encrypted_db_file_path,
+    );
 
-    // let _ = decrypt_database(&SecretString::from(argon2_password), &argon_salt, decrypted_db_file_path, encrypted_db_file_path)
+    // Decrypt the database
+    let _ = decrypt_database(
+        &SecretString::from(argon2_password),
+        decrypted_db_file_path,
+        encrypted_db_file_path,
+    );
 
-    // 4️⃣ Immediately zeroize the password from the RAM
-    // password.zeroize();
-    
     Ok(())
 }
